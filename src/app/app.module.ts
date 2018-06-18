a497d0fc2d01dff17233aac6ef827ec5e24df683
Mohammed Quaziar <mbaquirq@gmail.com>
2018-06-18 06:37:36 -0400

    completed tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,26 @@ import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule,
   MatSortModule } from '@angular/material';
 import { MyDashboardComponent } from './my-dashboard/my-dashboard.component';
 import { MyTableComponent } from './my-table/my-table.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FirstPageComponent } from './first-page/first-page.component';
+import { SecondPageComponent } from './second-page/second-page.component';
+import { ThirdPageComponent } from './third-page/third-page.component';
 
+
+const appRoutes: Routes = [
+  { path: 'first-page', component: FirstPageComponent },
+  { path: 'second-page', component: SecondPageComponent },
+  { path: 'third-page', component: ThirdPageComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
     MyNavComponent,
     MyDashboardComponent,
-    MyTableComponent
+    MyTableComponent,
+    FirstPageComponent,
+    SecondPageComponent,
+    ThirdPageComponent
   ],
   imports: [
     BrowserModule,
@@ -32,9 +45,11 @@ import { MyTableComponent } from './my-table/my-table.component';
     MatMenuModule,
     MatTableModule,
     MatPaginatorModule,
-    MatSortModule
+    MatSortModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
